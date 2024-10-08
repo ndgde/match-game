5b3db0e17394f0aaef960d0aa597533244364ef3
ndgde <ivanbujvol5@gmail.com>
2024-10-08 17:27:03 +0300

    rewriting game as a function

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,106 +1,159 @@
 /* eslint-disable react/jsx-key */
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Game.module.scss';
 import Grid from '../Grid/Grid';
-import Card from '../Card/Card';
+import Card, { CardState } from '../Card/Card';
 import Timer from '../Timer/Timer';
 import Button from '../Button/Button';
 import { v4 as uuidv4 } from 'uuid';
 
-class Game extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isGameOver: true,
-      cards: [],
-      prevId: null,
-      prevCallback: null,
-      timer: {},
-      namespace: '',
-    };
-    this.score = 0;
-  }
+const Game = () => {
+  const [isGameOver, setIsGameOver] = useState(true);
+  const [cards, setCards] = useState([]);
+  const [prevId, setPrevId] = useState(null);
+  const [timerI, setTimerI] = useState({
+    startTimer: () => {},
+    stopTimer: () => {},
+    resetTimer: () => {},
+    getElapsedTime: () => {},
+    getEasternTime: () => {},
+  });
+  const [namespace, setNamespace] = useState(null);
+  const [score, setScore] = useState(0);
+  const [numOfPairs, setNumOfPairs] = useState(0);
+  const [numOfErrors, setNumOfErrors] = useState(0);
 
-  generateCards = (numOfCards) => {
-    const cards = Array.from({ length: Math.floor(numOfCards / 2) }, (_, i) => [i, i])
-      .flat() /* returns [0, 0, 1, 1, 2, 2, ...] */
-      .sort(() => Math.random() - 0.5); /* random arrangement of card ids */
+  const genCardIndices = (numOfCards) =>
+    Array.from({ length: Math.floor(numOfCards / 2) }, (_, i) => [i, i])
+      .flat()
+      .sort(() => Math.random() - 0.5);
 
-    console.log(cards);
+  const genCards = (numOfCards) =>
+    genCardIndices(numOfCards).map((cardIndex, index) => ({
+      id: index,
+      index: cardIndex,
+      isFlipped: false,
+      isBlocked: false,
+      state: CardState.STANDARD,
+    }));
 
-    return cards;
-  };
+  useEffect(() => {
+    if (!isGameOver) {
+      setCards(genCards(12));
 
-  startGame = () => {
-    this.setState({
-      prevId: null,
-      prevCallback: null,
-      isGameOver: false,
-      cards: this.generateCards(12),
-      namespace: uuidv4(),
-    });
+      timerI.resetTimer();
+      setCards((prev) => prev.map((card) => ({ ...card, isFlipped: true, isBlocked: true })));
+      setTimeout(() => {
+        setCards((prev) => prev.map((card) => ({ ...card, isFlipped: false, isBlocked: false })));
+        timerI.startTimer();
+      }, 500);
 
-    this.score = 0;
-
-    this.state.timer.resetTimer();
-    this.state.timer.startTimer();
-  };
-
-  endGame = () => {
-    this.setState({ isGameOver: true });
-    this.state.timer.stopTimer();
-  };
-
-  increaseScore = () => {
-    this.score += 2; /* two cards at a time */
-    console.log(this.score);
-
-    if (this.score >= this.state.cards.length) {
-      this.endGame();
+      setPrevId(null);
+      setNamespace(uuidv4());
+      setScore(0);
+    } else {
+      setCards((prevCards) => prevCards.map((card) => ({ ...card, isBlocked: true })));
+      timerI.stopTimer();
     }
+  }, [isGameOver]);
+
+  // useEffect(() => {
+  //   console.log(cards);
+  // }, [cards]);
+
+  useEffect(() => {
+    if (numOfPairs >= cards.length) {
+      gameWon();
+    }
+  }, [numOfPairs]);
+
+  const startGame = () => setIsGameOver(false);
+  const endGame = () => setIsGameOver(true);
+  const gameWon = () => endGame();
+  // const getCard = (id) => cards[id];
+  // const getCardIndex = (id) => getCard(id).index;
+
+  const calcScore = () => {
+    const newScore = numOfPairs * 100 + Math.floor(timerI.getEasternTime() / 1000) - numOfErrors * 5;
+    setScore(newScore);
   };
 
-  cardClickHandler = (id, callback) => {
-    if (this.state.prevId !== null) {
-      if (id == this.state.prevId) {
-        [callback, this.state.prevCallback].forEach((func) => func(true));
-        this.increaseScore();
+  const clickHandler = (id) => {
+    const clickedCard = cards[id];
+
+    if (clickedCard.isBlocked || clickedCard.isFlipped) return;
+
+    setCards((prevCards) => {
+      const updatedCards = [...prevCards];
+      updatedCards[id].isFlipped = true;
+      updatedCards[id].isBlocked = true;
+
+      if (prevId !== null) {
+        const prevCard = updatedCards[prevId];
+
+        if (clickedCard.index === prevCard.index) {
+          updatedCards[id].state = CardState.CORRECT;
+          updatedCards[prevId].state = CardState.CORRECT;
+
+          setNumOfPairs((prev) => prev + 2);
+
+          updatedCards[id].isBlocked = false;
+          updatedCards[prevId].isBlocked = false;
+        } else {
+          setTimeout(() => {
+            setCards((newCards) =>
+              newCards.map((card, idx) =>
+                idx === id || idx === prevId
+                  ? { ...card, isFlipped: false, isBlocked: false, state: CardState.STANDARD }
+                  : card
+              )
+            );
+          }, 1000);
+
+          setNumOfErrors((prev) => prev + 1);
+
+          updatedCards[id].state = CardState.INCORRECT;
+          updatedCards[prevId].state = CardState.INCORRECT;
+        }
+
+        calcScore();
+        setPrevId(null);
       } else {
-        [callback, this.state.prevCallback].forEach((func) => func(false));
+        setPrevId(id);
       }
 
-      this.setState({ prevId: null, prevCallback: null });
-    } else {
-      this.setState({ prevId: id, prevCallback: callback });
-    }
-
-    this.forceUpdate();
+      return updatedCards;
+    });
   };
 
-  render() {
-    return (
-      <div className={`game ${styles.container}`}>
-        <Timer callback={(timer) => this.setState({ timer: timer })} onMilliseconds={true} />
-        {this.state.isGameOver ? (
-          <Button className={styles.game_btn} onClick={this.startGame}>
-            New Game
-          </Button>
-        ) : (
-          <Button className={styles.game_btn} onClick={this.endGame}>
-            Stop Game
-          </Button>
-        )}
-        <Grid
-          // width={4}
-          // height={3}
-          cards={this.state.cards.map((cardId) => (
-            <Card id={cardId} callback={this.cardClickHandler} isGameActive={!this.state.isGameOver} />
-          ))}
-          namespace={this.state.namespace}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className={`game ${styles.container}`}>
+      <Timer
+        callback={(timerI) => setTimerI(timerI)}
+        timeAmount={5 * 60 * 1000}
+        endTrigger={endGame}
+        onMilliseconds={true}
+      />
+      {isGameOver ? (
+        <Button className={styles.game_btn} onClick={startGame}>
+          New Game
+        </Button>
+      ) : (
+        <Button className={styles.game_btn} onClick={endGame}>
+          Stop Game
+        </Button>
+      )}
+      <p>{score}</p>
+      <Grid
+        // width={4}
+        // height={3}
+        cards={cards.map(({ id, index, isFlipped, state }) => (
+          <Card key={id} id={id} index={index} isFlipped={isFlipped} state={state} onClick={() => clickHandler(id)} />
+        ))}
+        namespace={namespace}
+      />
+    </div>
+  );
+};
 
 export default Game;
