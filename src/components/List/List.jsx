97eefa9fe65246f3ccdad89bc84027e2ba145390
ndgde <ivanbujvol5@gmail.com>
2024-10-10 17:34:46 +0300

    added html layout

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './List.module.scss';
 
-const List = ({ items, renderItem, className = '', style = {} }) => {
+const List = ({ items, renderItem, className = '', itemClassName, style = {} }) => {
   return (
     <ul className={`${styles.list} ${className}`} style={style}>
       {items.map((item, index) => (
-        <li className={styles.item} key={index}>
+        <li className={`${styles.item} ${itemClassName}`} key={index}>
           {renderItem(item, index)}
         </li>
       ))}
@@ -18,6 +18,7 @@ List.propTypes = {
   items: PropTypes.array.isRequired,
   renderItem: PropTypes.func.isRequired,
   className: PropTypes.string,
+  itemClassName: PropTypes.string,
   style: PropTypes.object,
 };
 
--- src/pages/GameSettings/GameSettings.jsx
+++ src/pages/GameSettings/GameSettings.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import styles from './GameSettings.module.scss';
 import Header from '../../components/Header/Header';
 import List from '../../components/List/List';
 import Layout from '../../components/Layout/Layout';
+import Main from '../../components/Main/Main';
+import UserIcon from '../../components/UserIcon/UserIcon';
 
 const GameSettings = () => {
   const settings = [
@@ -10,19 +13,31 @@ const GameSettings = () => {
   ];
 
   return (
-    <Layout
-      header={<Header />}
-      content={
-        <>
-          <h1>Настройки игры</h1>
-          <List>
-            {settings.map((setting) => (
-              <li key={setting.id}>{setting.name}</li>
-            ))}
-          </List>
-        </>
-      }
-    />
+    <Layout>
+      <>
+        <Header user={<UserIcon />} />
+        <Main>
+          <div className={styles.container}>
+            <List
+              items={settings}
+              itemClassName={styles.item}
+              renderItem={(setting) => (
+                <div className={styles.item_content}>
+                  <label htmlFor="dropdown">{setting.name}</label>
+                  <div className={styles.custom_select_wrapper}>
+                    <select id="dropdown" className={styles.select}>
+                      <option value="option1">Option 1</option>
+                      <option value="option2">Option 2</option>
+                      <option value="option3">Option 3</option>
+                    </select>
+                  </div>
+                </div>
+              )}
+            />
+          </div>
+        </Main>
+      </>
+    </Layout>
   );
 };
 
