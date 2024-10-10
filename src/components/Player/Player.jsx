84c1e21a2a3108da7f3b5d6db57151958f8a129f
ndgde <ivanbujvol5@gmail.com>
2024-10-10 16:18:36 +0300

    styles for best score page added

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Player.module.scss';
 
-const Player = ({ name, score, rank, avatar, className = '', style = {} }) => {
+const Player = ({ name, score, email, avatar, className = '', style = {} }) => {
   return (
     <div className={`player ${styles.container} ${className}`} style={style}>
       <img className={styles.avatar} src={avatar} alt={`${name}'s avatar`} />
       <div className={styles.info}>
-        <h3 className={styles.name}>{name}</h3>
-        <p className={styles.score}>Score: {score}</p>
-        <p className={styles.rank}>Rank: {rank}</p>
+        <div className={styles.left_section}>
+          <h3 className={styles.name}>{name}</h3>
+          <p className={styles.email}>{email}</p>
+        </div>
+        <div className={styles.right_section}>
+          <p className={styles.score}>
+            Score: <span className={styles.score_value}>{score}</span>
+          </p>
+        </div>
       </div>
     </div>
   );
@@ -18,7 +24,7 @@ const Player = ({ name, score, rank, avatar, className = '', style = {} }) => {
 Player.propTypes = {
   name: PropTypes.string.isRequired,
   score: PropTypes.number.isRequired,
-  rank: PropTypes.number.isRequired,
+  email: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
   className: PropTypes.string,
   style: PropTypes.object,
--- src/pages/BestScores/BestScores.jsx
+++ src/pages/BestScores/BestScores.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './BestScores.module.scss';
 import Header from '../../components/Header/Header';
 import List from '../../components/List/List';
 import Player from '../../components/Player/Player';
@@ -8,9 +9,9 @@ import UserIcon from '../../components/UserIcon/UserIcon';
 
 const BestScores = () => {
   const players = [
-    { id: 1, name: 'Игрок 1', score: 100 },
-    { id: 2, name: 'Игрок 2', score: 90 },
-  ];
+    { id: 1, name: 'Игрок 1', score: 2000, email: 'some-email1@mail.com' },
+    { id: 2, name: 'Игрок 2', score: 1500, email: 'some-email2@mail.com' },
+  ].sort((a, b) => b.score - a.score);
 
   return (
     <Layout>
@@ -18,10 +19,17 @@ const BestScores = () => {
         <Header user={<UserIcon />} />
         <Main>
           <>
-            <h1>Лучшие игроки</h1>
+            <h1 className={styles.title}>Best players</h1>
             <List
               items={players}
-              renderItem={(player) => <Player name={player.name} score={player.score} rank={2} avatar={'0'} />}
+              renderItem={(player) => (
+                <Player
+                  name={player.name}
+                  score={player.score}
+                  email={player.email}
+                  avatar={`${process.env.PUBLIC_URL}/unauthenticated-user.png`}
+                />
+              )}
             />
           </>
         </Main>
